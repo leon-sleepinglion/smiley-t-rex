1954b6c10cb48b420c6e9de3e0a28e768b9360af
Leon Wee <leonweecs@gmail.com>
2020-11-29 00:27:20 +1300

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Smiley T-Rex
+
+Combining the power of face detection + expression reconigtion and the little T-Rex Runner game we enjoyed on Google Chrome when your device is offline.
+
+Just smile to play. 😁😁😁
+
+## Credits
+
+- [face-api.js](https://github.com/justadudewhohacks/face-api.js/)
+- [t-rex-runner](https://github.com/wayou/t-rex-runner)
---
+++ control.js
@@ -0,0 +1,69 @@
+document.onkeydown = function (evt) {
+    evt = evt || window.event;
+    if (evt.keyCode == 32) {
+        var box = document.getElementById("messageBox");
+        box.style.visibility = "hidden";
+    }
+};
+
+$(document).ready(function () {
+    alert('Welcome fo the Smiley T-Rex Runner\n\n😁 Smile to your camera to play the game.\n😡Make an angry face to restart the game when you lose.')
+    run()
+})
+
+async function run() {
+    // load the model
+    await faceapi.loadTinyFaceDetectorModel('/models')
+    await faceapi.loadFaceExpressionModel('/models')
+
+    // Try to access users webcam and stream the images
+    const videoEl = document.getElementById('inputVideo')
+    navigator.getUserMedia(
+        { video: {} },
+        stream => videoEl.srcObject = stream,
+        err => console.error(err)
+    )
+}
+
+async function onPlay(videoEl) {
+    const options = new faceapi.TinyFaceDetectorOptions({ inputSize: 256, scoreThreshold: 0.5 })
+
+    const result = await faceapi.detectSingleFace(videoEl, options).withFaceExpressions()
+
+    if (result) {
+        const canvas = $('#overlay').get(0)
+        const dims = faceapi.matchDimensions(canvas, videoEl, true)
+
+        const resizedResult = faceapi.resizeResults(result, dims)
+        const minConfidence = 0.5
+
+        faceapi.draw.drawDetections(canvas, resizedResult)
+        faceapi.draw.drawFaceExpressions(canvas, resizedResult, minConfidence)
+
+        if (result.expressions.happy > 0.7) {
+            // Simulate the action of pressing down spacebar
+            var e = new KeyboardEvent("keydown", {
+                bubbles: true,
+                cancelable: true,
+                key: " ",
+                shiftKey: true,
+                keyCode: 32
+            })
+            document.dispatchEvent(e)
+        }
+
+        else if (result.expressions.angry > 0.7) {
+            // Simulate the action of pressing down spacebar
+            var e = new KeyboardEvent("keyup", {
+                bubbles: true,
+                cancelable: true,
+                key: " ",
+                shiftKey: true,
+                keyCode: 32
+            })
+            document.dispatchEvent(e)
+        }
+    }
+
+    setTimeout(() => onPlay(videoEl))
+}
